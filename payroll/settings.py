41ca4ca1b39934289b4074e4bcbc5ae897c45d43
Simon Kamau <simonkamau690@gmail.com>
2023-07-26 12:57:31 +0300

    pwa
    introduced pwa

--- payroll/settings.py
+++ payroll/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "bootstrap5",
     'first',
     'pwa',
+    'django_pwa',
     #'accounts',
 ]
 MIDDLEWARE = [
@@ -162,32 +163,32 @@ VERIFICATION_CODE_VALIDITY_IN_MINUTES = 1 # range of [1, 60] only
 # RATELIMIT_RATE = '10/m'
 
 #pwa
-PWA_APP_NAME = 'My App'
-PWA_APP_DESCRIPTION = "My app description"
-PWA_APP_THEME_COLOR = '#0A0302'
-PWA_APP_BACKGROUND_COLOR = '#ffffff'
-PWA_APP_DISPLAY = 'standalone'
-PWA_APP_SCOPE = '/'
-PWA_APP_ORIENTATION = 'any'
-PWA_APP_START_URL = '/'
-PWA_APP_STATUS_BAR_COLOR = 'default'
-PWA_APP_ICONS = [
+PWA_NAME = 'test app'
+PWA_SHORT_NAME = 'test_app'
+PWA_ICONS = [
     {
-        'src': '/static/images/my_app_icon.png',
-        'sizes': '160x160'
+        "src": "/static/icons/icon-128x128.png",
+        "sizes": "128x128",
+        "type": "image/png"
+    }, {
+        "src": "/static/icons/icon-144x144.png",
+        "sizes": "144x144",
+        "type": "image/png"
+    }, {
+        "src": "/static/icons/icon-152x152.png",
+        "sizes": "152x152",
+        "type": "image/png"
+    }, {
+        "src": "/static/icons/icon-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+    }, {
+        "src": "/static/icons/icon-256x256.png",
+        "sizes": "256x256",
+        "type": "image/png"
+    }, {
+        "src": "/static/icons/icon-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
     }
 ]
-PWA_APP_ICONS_APPLE = [
-    {
-        'src': '/static/images/my_apple_icon.png',
-        'sizes': '160x160'
-    }
-]
-PWA_APP_SPLASH_SCREEN = [
-    {
-        'src': '/static/images/icons/splash-640x1136.png',
-        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
-    }
-]
-PWA_APP_DIR = 'ltr'
-PWA_APP_LANG = 'en-US'
