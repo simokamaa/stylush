358690b8b79447067833f3fc673b6527eab33aa9
Simon Kamau <simonkamau690@gmail.com>
2023-07-31 16:24:32 +0300

    pwa setup

--- payroll/settings.py
+++ payroll/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
     os.path.join(BASE_DIR, 'static'),
 ]
 
@@ -161,33 +160,47 @@ VERIFICATION_CODE_VALIDITY_IN_MINUTES = 1 # range of [1, 60] only
 # RATELIMIT_KEY_FUNCTION = 'ratelimit.utils.get_rate_limit_key'
 # RATELIMIT_RATE = '10/m'
 
-#pwa
-PWA_NAME = 'test app'
-PWA_SHORT_NAME = 'test_app'
-PWA_ICONS = [
+
+PWA_APP_NAME = 'My App'
+PWA_APP_DESCRIPTION = "My app description"
+PWA_APP_THEME_COLOR = '#0A0302'
+PWA_APP_BACKGROUND_COLOR = '#ffffff'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
     {
-        "src": "/static/icons/icon-128x128.png",
-        "sizes": "128x128",
-        "type": "image/png"
-    }, {
-        "src": "/static/icons/icon-144x144.png",
-        "sizes": "144x144",
-        "type": "image/png"
-    }, {
-        "src": "/static/icons/icon-152x152.png",
-        "sizes": "152x152",
-        "type": "image/png"
-    }, {
-        "src": "/static/icons/icon-192x192.png",
-        "sizes": "192x192",
-        "type": "image/png"
-    }, {
-        "src": "/static/icons/icon-256x256.png",
-        "sizes": "256x256",
-        "type": "image/png"
-    }, {
-        "src": "/static/icons/icon-512x512.png",
-        "sizes": "512x512",
-        "type": "image/png"
+        'src': '/static/images/my_app_icon.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/images/my_apple_icon.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/images/icons/splash-640x1136.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
+PWA_APP_SHORTCUTS = [
+    {
+        'name': 'Shortcut',
+        'url': '/target',
+        'description': 'Shortcut to a page in my application'
+    }
+]
+PWA_APP_SCREENSHOTS = [
+    {
+      'src': '/static/images/icons/splash-750x1334.png',
+      'sizes': '750x1334',
+      "type": "image/png"
     }
 ]
