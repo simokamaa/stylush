3b53aac801dc5a3635ce4403fa27636fbbf27d10
Simon Kamau <simonkamau690@gmail.com>
2023-08-02 12:26:53 +0300

    urls change
    slight modifications

--- payroll/urls.py
+++ payroll/urls.py
@@ -23,11 +23,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',   views.login),
     path('admin/', admin.site.urls),
-    path('home/', include('first.urls'))
+    path('home/', include('first.urls')),
+    path('', include('pwa.urls'))
 ]
 
 # Serving media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 
